828e99c56ff206a6f71dfca22e38bbea19ac87d8
lai <a2323178080@gmail.com>
2021-12-20 01:48:35 +0800

    上傳new-project

---
+++ src/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </div>
+  <router-view/>
+  
+  <button type="button" class="btn btn-primary">Primary</button>
+  <button type="button" class="btn btn-secondary">Secondary</button>
+  <button type="button" class="btn btn-success">Success</button>
+  <button type="button" class="btn btn-danger">Danger</button>
+  <button type="button" class="btn btn-warning">Warning</button>
+  <button type="button" class="btn btn-info">Info</button>
+  <button type="button" class="btn btn-light">Light</button>
+  <button type="button" class="btn btn-dark">Dark</button>
+
+  <button type="button" class="btn btn-link">Link</button>
+
+  {{name}}
+</template>
+
+
+<script>
+export default{
+  data:function(){
+    return{
+      name:process.env.VUE_APP_PATH
+    };
+  },
+}
+</script>
+
+
+<style lang="scss">
+@import "bootstrap"
+
+</style>
+
+
---
+++ src/views/NewPage.vue
@@ -0,0 +1,3 @@
+<template>
+    <div>新增葉面</div>
+</template>
